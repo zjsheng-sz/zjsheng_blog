f316dd4c550e1df7019629cd93503ecba9a6b121
zjsrose <770649662@qq.com>
2018-06-23 09:59:30 +0000

    Deploy to GitHub Pages by travis

--- C/arithmeticBook/3_queue.c
+++ C/arithmeticBook/3_queue.c
@@ -16,53 +16,117 @@ struct QueueRecord{
 
 int IsEmpty(Queue Q){
 
-
+  return Q->size == 0;
 }
 
 
 int IsFull(Queue Q){
 
+  return Q->size == Q->capacity;
+}
 
+void MakeEmpty(Queue Q){
+
+  Q->size = 0;
+  Q->front = 1;
+  Q->rear = 0;
 }
 
 
 Queue CreateQueue(int MaxElements){
 
+    Queue Q;
 
+    if (MaxElements < MinQueueSize) {
+      faltalError("too small");
+    }
+    else{
+
+      Q = malloc(sizeof(struct QueueRecord));
+
+      if (Q == NULL) {
+        faltalError("out of space");
+      }else{
+
+        Q->array = malloc(sizeof(ElementType) * Q->capacity);
+        if (!Q->array) {
+          faltalError("out of space");
+        }else{
+          MakeEmpty(Q)
+        }
+      }
+    }
+
+    return Q;
 }
 
 
 void DisposeQueue(Queue Q){
 
-
+  if (Q) {
+    free(Q->array);
+    free(Q);
+  }
 }
 
+static int Succ(int Value, Queue Q){
 
-void MakeEmpty(Queue Q){
+  if (++Value == Q->capacity) {
+    Value == 0;
+  }
 
-
+  return Value;
 }
 
 
 void EnQueue(ElementType X, Queue Q){
 
+  if (IsFull(Q)) {
+    faltalError("full Queue");
+  }
+  else{
+
+    Q->size ++;
+    Q->rear == Succ((Q->rear + 1), Q);
+    Q->array[Q->rear] = X;
+
+  }
 
 }
 
 
 ElementType Front(Queue Q){
 
-
+  return Q->array[Q->front];
 }
 
 
 void DeQueue(Queue Q){
 
+  if (IsEmpty(Q)) {
+    faltalError("empty Queue");
+  }
+  else{
 
+    Q->size --;
+    Q->front == Succ((Q->rear + 1), Q);
+    // Q->array[Q->front] = X;
+
+  }
 }
 
 
 ElementType FrontAndDequeue(Queue Q){
 
+  if (IsEmpty(Q)) {
+    faltalError("empty Queue");
+    return -1;
+  }
+  else{
+
+    Q->size --;
+    Q->front == Succ((Q->rear + 1), Q);
+    return Q->array[Q->front]
+  }
 
 }
