1e16461ca5012e3b0ccb12c3215e60aebd7493a4
zjsrose <770649662@qq.com>
2018-06-23 09:36:35 +0000

    Deploy to GitHub Pages by travis

--- C/arithmeticBook/3_queue.h
+++ C/arithmeticBook/3_queue.h
@@ -1,1 +1,15 @@
 //和栈一样，队列也是表。然而，使用队列时插入在一端进行而删除则在另一端进行。
+
+//前出后进
+struct QueueRecord;
+typedef struct QueueRecord *Queue;
+
+int IsEmpty(Queue Q);
+int IsFull(Queue Q);
+Queue CreateQueue(int MaxElements);
+void DisposeQueue(Queue Q);
+void MakeEmpty(Queue Q);
+void EnQueue(ElementType X, Queue Q);
+ElementType Front(Queue Q);
+void DeQueue(Queue Q);
+ElementType FrontAndDequeue(Queue Q);
